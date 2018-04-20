f58ac14e7ab34fd3731d21302cd76f064375602a
Erlang Parasu <erlangparasu@protonmail.com>
2018-04-21 07:17:21 +0800

    node

--- server.js
+++ server.js
@@ -4,16 +4,6 @@ const express = require('express');
 const WebSocket = require('ws');
 const path = require('path');
 
-const wss = new WebSocket.Server({ port: 8080 });
-var s = [];
-wss.on('connection', function connection(ws) {
-  s.push(ws);
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
-  ws.send('something');
-});
-
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
@@ -28,3 +18,11 @@ app.get('/api', (req, res) => {
     res.send('Build signal accepted!');
 });
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const wss = new WebSocket.Server({ port: 8080 });
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+  ws.send('something');
+});
