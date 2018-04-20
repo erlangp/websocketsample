0ae6cdc6e58a3ba9c39bad7210bc1b11f102fb7e
Erlang Parasu <erlangparasu@protonmail.com>
2018-04-21 07:57:54 +0800

    node

--- server.js
+++ server.js
@@ -3,9 +3,9 @@
 var express = require('express');
 var WebSocket = require('ws');
 var path = require('path');
+var http = require('http');
 
 var PORT = process.env.PORT || 3000;
-PORT = 3000;
 var INDEX = path.join(__dirname, 'index.html');
 
 var app = express();
@@ -13,16 +13,24 @@ app.get('/', function (req, res) {
     res.send('Root Hello World!');
 });
 app.get('/api', function (req, res) {
-//    wss.clients.forEach((client) => {
-//        client.send(new Date().toTimeString());
-//    });
+    // wss.clients.forEach((client) => {
+        // client.send(new Date().toTimeString());
+    // });
     res.send('API Build signal accepted!');
 });
-app.listen(PORT, function () {
-  console.log('Ready');
+// app.listen(PORT, function () {
+  // console.log('Ready');
+// });
+
+var server = http.createServer(function (req, res) {
+    // process HTTP request. Since we're writing just WebSockets
+    // server we don't have to implement anything.
+});
+server.listen(PORT, function () {
+    console.log('Ready');
 });
 
-var wss = new WebSocket.Server({port: 4000});
+var wss = new WebSocket.Server({server: server});
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
