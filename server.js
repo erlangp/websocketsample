45c587f381f0dfc012cd95dc16fcd878f171fd50
Erlang Parasu <erlangparasu@protonmail.com>
2018-04-21 07:38:07 +0800

    node

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
 'use strict';
 
-const express = require('express');
-const WebSocket = require('ws');
-const path = require('path');
+var express = require('express');
+var WebSocket = require('ws');
+var path = require('path');
 
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+var PORT = process.env.PORT || 3000;
+var INDEX = path.join(__dirname, 'index.html');
 
-const app = express();
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+var app = express();
+app.get('/', function (req, res) {
+    res.send('Root Hello World!');
 });
-app.get('/api', (req, res) => {
+app.get('/api', function (req, res) {
 //    wss.clients.forEach((client) => {
 //        client.send(new Date().toTimeString());
 //    });
-    res.send('Build signal accepted!');
+    res.send('API Build signal accepted!');
+});
+app.listen(PORT, function () {
+  console.log('Ready');
 });
 
-const wss = new WebSocket.Server();
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
-  ws.send('something');
-});
-
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+// const wss = new WebSocket.Server();
+// wss.on('connection', function connection(ws) {
+    // ws.on('message', function incoming(message) {
+        // console.log('received: %s', message);
+    // });
+    // ws.send('something');
+// });
