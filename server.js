062888ae69a684da4b43a08f9cccfa4fe473ea64
Erlang Parasu <erlangparasu@protonmail.com>
2018-04-21 06:58:10 +0800

    node

--- server.js
+++ server.js
@@ -10,14 +10,10 @@ const INDEX = path.join(__dirname, 'index.html');
 const server = express()
 const wss = new SocketServer({ server });
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
+server.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
-server.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 server.get('/api', (req, res) => {
 	wss.clients.forEach((client) => {
 		client.send(new Date().toTimeString());
@@ -32,3 +28,8 @@ setInterval(() => {
 }, 1000);
 
 server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+    ws.on('close', () => console.log('Client disconnected'));
+});
