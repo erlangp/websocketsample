5ae695a26eaeaf2babac46101d9760fc17f61af4
Erlang Parasu <erlangparasu@protonmail.com>
2018-04-21 07:40:54 +0800

    node

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.listen(PORT, function () {
   console.log('Ready');
 });
 
-var wss = new WebSocket.Server();
+var wss = new WebSocket.Server({port: 8080});
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
