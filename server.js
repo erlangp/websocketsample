5180fc1ede051be9fb0c8618648a7fd6ae78899c
Erlang Parasu <erlangparasu@protonmail.com>
2018-04-21 09:47:48 +0800

    My WebSocket Tools To Trigger Build

--- server.js
+++ server.js
@@ -10,41 +10,49 @@ var INDEX = path.join(__dirname, 'index.html');
 
 var app = express();
 app.get('/', (req, res) => {
-    res.send('root. Hello World!' + ' :' + PORT);
+    res.send('Root' + ' :' + PORT);
 });
-app.get('/api', (req, res) => {
-    wss.clients.forEach((client) => {
-        client.send(JSON.stringify(client));
-//        client.send(client, 'Amazing ' + new Date().toTimeString());
-    });
-    res.send('api. Hello World!' + ' :' + PORT);
+app.get('/api/do-build', (req, res) => {
+    try {
+        if (wss) {
+            if (wss.clients) {
+                wss.clients.forEach((client) => {
+                    if (client.readyState === WebSocket.OPEN) {
+                        client.send(JSON.stringify({
+                            code: "DO_BUILD", 
+                            time: new Date().toTimeString()
+                        }));
+                    }
+                });
+            }
+        }
+    } catch (er) {
+        //
+    }
+    res.send('Api Build' + ' :' + PORT);
 });
-// app.listen(PORT, function () {
-  // console.log('App Ready!');
-// });
+//app.listen(PORT, function () {
+//    console.log('App Ready!');
+//});
 
 var server = http.createServer(app);
 
 var wss = new WebSocket.Server({
     server: server
 });
-
 wss.on('connection', (ws) => {
     console.log('onConnected: ');
     
     ws.on('open', () => {
         console.log('onOpen: ');
-        ws.send(Date.now());
     });
     
     ws.on('message', (message) => {
-        console.log('onMessage: incomingMessage= %s', message);
-        ws.send(Date.now());
+        console.log('onMessage: incomingMessage=%s', message);
     });
     
     ws.on('close', () => {
         console.log('onClose: ');
-        ws.send(Date.now());
     });
 
     ws.send('Something', (error) => {
